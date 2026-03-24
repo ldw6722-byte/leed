9047e57cf4865d66edfd3d8f9ab523943c023a49
leed <ldw6722@gmail.com>
2026-03-24 18:18:15 +0900

    pyt

--- python/jung.py
+++ python/jung.py
@@ -1,2 +1,63 @@
 print('Hello_Jungo!')
 print('Hi Python')
+print('My second program.')
+print(2)
+print('programs')
+
+print('I have')
+print(3)
+print('books.')
+
+print('My age:')
+print(15)
+print('My weight:')
+print(67.3)
+
+print('''I have 
+3 
+books.''')
+
+print('''My age:
+15
+My weight:
+67.3''')
+
+print('Program number' , '3')
+print('Program number' , '3')
+
+print('Apple' , 'Banana' , 'Carrot')
+
+print("*   *")
+print(" * * ")
+print("  *")
+
+letter = 'how are YOU'
+print(letter.lower())
+print(letter.upper())
+print(letter.capitalize())
+print(letter.title())
+print(letter.swapcase())
+print(letter.split())
+
+print(letter.count('how'))
+
+s = '나도고등학교'
+print(s.startswith('나도'))
+print(s.endswith('초등학교'))
+
+s2 = '...나도고등학교...'
+print(s.strip('.'))
+
+s3 = '.,.나도고등학교.,.'
+print(s3.strip('.'))
+
+s4 = '나도고등학교'
+print(s4.replace('고등학교', '고교'))
+
+s5 = '나도고교너도고교'
+print(s5.replace고교', '고등학교'))
+
+len('pulljima')
+
+
+
---
+++ python/py3.py
@@ -0,0 +1,8 @@
+A = int(input())
+B = int(input())
+
+print(A * (B % 10))        
+print(A * ((B // 10) % 10))  
+print(A * (B // 100))
+print(A * B)
+
