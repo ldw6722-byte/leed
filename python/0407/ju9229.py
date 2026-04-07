ccb505a2b64a9763e5e3242d8aacc1ecb1737be2
leed <ldw6722@gmail.com>
2026-04-07 18:40:24 +0900

    0407

---
+++ python/0407/ju9229.py
@@ -0,0 +1,5 @@
+a = int(input())
+if a >= 13:
+    print('Elementary School')
+else:
+    print('Middle School')
---
+++ python/0407/ju9386.py
@@ -0,0 +1,5 @@
+pi = 3.14
+half = int(input())
+Area = half * half * pi
+print(f"PI = {pi:.2f}")
+print(f"Area = {half} * {half} * {pi} = {Area:.1f}")
