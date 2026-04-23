a5944842ee9ad46dab3ba08fbf80fda41e36eb6a
leed <ldw6722@gmail.com>
2026-04-23 17:53:30 +0900

    0423

--- work-react/index.js
+++ work-react/index.js
@@ -1,1 +1,8 @@
-console.log("index run");
+import lodash from "lodash"; 
+
+const arr = [1, 1, 1, 2, 2, 1, 1, 4, 4, 3, 2];
+const uniqueArr = lodash.uniqBy(arr);
+
+console.log(uniqueArr);
+
+// [1, 2, 4, 3]
